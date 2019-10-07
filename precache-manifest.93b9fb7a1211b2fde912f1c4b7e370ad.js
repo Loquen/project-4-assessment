a44a9402487b12580ed863f3d15b75bf82de17d8
Loquen Jones <lrjones416@gmail.com>
2019-10-07 14:01:12 -0700

    Updates

---
+++ precache-manifest.93b9fb7a1211b2fde912f1c4b7e370ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead7ce7ac17ee8ce66bd318291c17bf8",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "615fc1bc8fd4b02d3883",
+    "url": "/project-4-assessment/static/css/main.c04c135c.chunk.css"
+  },
+  {
+    "revision": "adcc6c9126018b755d0a",
+    "url": "/project-4-assessment/static/js/2.25804772.chunk.js"
+  },
+  {
+    "revision": "615fc1bc8fd4b02d3883",
+    "url": "/project-4-assessment/static/js/main.c6a5b8b4.chunk.js"
+  },
+  {
+    "revision": "75de8ec33a8a78802050",
+    "url": "/project-4-assessment/static/js/runtime-main.71652b97.js"
+  }
+]);
