51f1820c5b4b78c6dfabb6416ea9b0817ac2bb93
Loquen Jones <lrjones416@gmail.com>
2019-10-07 14:05:07 -0700

    change ternary to simplify

--- src/CircleSelector/CircleSelector.jsx
+++ src/CircleSelector/CircleSelector.jsx
@@ -5,9 +5,7 @@ const CircleSelector = (props) => {
   return (
     <div className='CircleSelector'>
       {props.btns.map((number) => 
-        parseInt(props.selected) === number
-        ? <button onClick={props.selectBtn} key={number} name={number} className='selected'> Select Circle {number} </button>
-        : <button onClick={props.selectBtn} key={number} name={number}> Select Circle {number} </button>
+        <button onClick={props.selectBtn} key={number} name={number} className={parseInt(props.selected) === number ? 'selected' : ''}> Select Circle {number} </button>
       )}
 
     </div>
--- src/Circles/Circles.jsx
+++ src/Circles/Circles.jsx
@@ -5,9 +5,7 @@ const Circles = (props) => {
   return (
     <div className='Circles'>
       {props.btns.map((number) => 
-        parseInt(props.selected) === number
-        ? <div onClick={props.selectBtn} key={number} name={number} className='selected'> {number} </div>
-        : <div onClick={props.selectBtn} key={number} name={number}> {number} </div>
+        <div onClick={props.selectBtn} key={number} name={number} className={parseInt(props.selected) === number ? 'selected' : ''}> {number} </div>
       )}
     </div>
   );
